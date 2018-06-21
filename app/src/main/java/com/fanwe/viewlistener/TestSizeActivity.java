6270096556db3c4c76ec8f85a8117f79bfef1b56
zjun <565061763@qq.com>
2018-06-21 17:19:26 +0800

    demo

---
+++ app/src/main/java/com/fanwe/viewlistener/TestSizeActivity.java
@@ -0,0 +1,53 @@
+package com.fanwe.viewlistener;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.fanwe.lib.viewlistener.FViewSizeListener;
+
+public class TestSizeActivity extends AppCompatActivity
+{
+    public static final String TAG = TestSizeActivity.class.getSimpleName();
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_size);
+        mButton = findViewById(R.id.btn);
+        mButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                final ViewGroup.LayoutParams params = v.getLayoutParams();
+                params.width = v.getWidth() + 10;
+                params.height = v.getHeight() + 10;
+                v.setLayoutParams(params);
+            }
+        });
+
+        mListener.setView(mButton);
+    }
+
+    private final FViewSizeListener<Button> mListener = new FViewSizeListener<Button>()
+    {
+        @Override
+        protected void onWidthChanged(int newWidth, int oldWidth, View view)
+        {
+            Log.i(TAG, "onWidthChanged:" + newWidth + "," + oldWidth);
+        }
+
+        @Override
+        protected void onHeightChanged(int newHeight, int oldHeight, View view)
+        {
+            Log.i(TAG, "onHeightChanged:" + newHeight + "," + oldHeight);
+        }
+    };
+}
