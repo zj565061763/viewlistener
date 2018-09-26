b587b67472cad10aee6830aac58d0ead36f85f20
zjun <565061763@qq.com>
2018-09-26 15:41:48 +0800

    demo

--- app/src/main/java/com/sd/viewlistener/MainActivity.java
+++ app/src/main/java/com/sd/viewlistener/MainActivity.java
@@ -1,27 +1,33 @@
 package com.sd.viewlistener;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
-    private Button mButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mButton = findViewById(R.id.btn);
-        mButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
+    }
 
-            }
-        });
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.btn_size:
+                startActivity(new Intent(this, TestSizeActivity.class));
+                break;
+            case R.id.btn_visibility:
+                startActivity(new Intent(this, TestVisibilityActivity.class));
+                break;
+            case R.id.btn_selection:
+                startActivity(new Intent(this, TestSelectionActivity.class));
+                break;
+        }
     }
 }
